71b1aa3c68ec7b23f3dab3e26dfe9d2e53a45b4d
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2025-07-26 17:45:20 -0300

    gitmal

--- scripts/nosotros.js
+++ scripts/nosotros.js
@@ -32,8 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
 document.querySelectorAll('.reveal').forEach(el => obs.observe(el));
 
 
-  placeAuthButtons();
-  window.addEventListener('resize', placeAuthButtons);
 
 
 });
